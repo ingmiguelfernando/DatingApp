31c5ba2e66ee614311e991bbd383b19d87904b93
Miguel Fernando Patiño <ing.miguel.fernando@gmail.com>
2018-10-01 20:53:22 -0500

    Ajustes código

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    //Dtos = Data transfer Objects (objetos parciales que reciben los controladores)
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
