1e6d3744cd521b156aaec55a0a520783780ac8a6
Miguel Fernando Patiño <ing.miguel.fernando@hotmail.com>
2018-10-04 08:06:10 -0500

    fin api

--- DatingApp.API/Dtos/UserForLoginDto.cs
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Dtos
 {
-    //Dtos = Data transfer Objects (objetos parciales que reciben los controladores)
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
-        [Required]
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
-
-
     }
 }
