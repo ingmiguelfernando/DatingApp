d3ac3c644a2ecb2e54b15760a56806e32d01452d
Miguel Fernando Patiño <ing.miguel.fernando@hotmail.com>
2018-10-05 20:23:36 -0500

    alertifyjs

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,22 +12,21 @@ export class RegisterComponent implements OnInit {
   model: any = {};
 
 
-constructor(private authService: AuthService) { }
+constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.Register(this.model).subscribe(() => {
-      console.log('Registration successuful');
+      this.alertify.success('Registration successuful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 
 
