f09f3d182503d38d7e3ebf0c74aff92fd7017cf9
Miguel Fernando Patiño <ing.miguel.fernando@hotmail.com>
2018-10-07 21:12:33 -0500

    Ajustes del Api

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,19 @@ namespace DatingApp.API.Models
         public string  Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string  Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }       
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }        
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+
+
     }
 }
